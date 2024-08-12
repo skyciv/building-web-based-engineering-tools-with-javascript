2108a7ce53155378b428fa1c0037375c33c50401
GeorgeChard <george.chard@skyciv.com>
2024-08-12 11:06:27 +1000

    Pushed for JR review of SFD graph

--- calculate.js
+++ calculate.js
@@ -5,9 +5,8 @@ module.exports = async function (input_json) {
 	//Calculations
 	let reaction_force = (w * L)/(2 * 1000)
 	let peak_moment = (w * L**2)/(8 * 1000)
-	let peak_shear = reaction_force
 
-	if(calculate_deflection){
+	if(calculate_deflection){ //Only run this code if the user has selected Calculate Deflection
 		var peak_deflection = (5 * w * L**4)/(384 * E * I_z)
 		var span_ratio = Math.round(L / peak_deflection);
 		var span_ratio_string = "L/" + span_ratio
@@ -16,15 +15,15 @@ module.exports = async function (input_json) {
 	//Reporting
 	ReportHelpers.print(`Reaction<br>Force`,`[math]Reaction = w*L/2 = ${reaction_force} kN[math]`, ``)
 	ReportHelpers.print(`Peak<br>Moment`,`[math]M^* = \\frac{w*L^2}{8} = ${peak_moment} kNm[math]`, ``)
-	ReportHelpers.print(`Peak<br>Shear`,`[math]V^* = ${peak_shear} kN[math]`, ``)
 	ReportHelpers.print(`Peak<br>Deflection`,`[math]Deflection = \\frac{5*w*L^4}{384 * E * I_{z}} = ${peak_deflection}mm[math]`, ``)
 	ReportHelpers.print(`Span<br>Ratio`,`[math]Ratio = \\frac{L}{${span_ratio}}[math]`, ``)
 
 	ReportHelpers.drawGraph({
 		loads_array: [0.3, 0.3, 0.3, [0.3, -0.3], -0.3, -0.3],
-		member_length: 12.23,
+		member_length: L,
 		loads_unit: "kN", 
-		result_label_: "SFD",
+		result_label_z: "SFD",
+		result_label_y: "SFD",
 		result_label_x: "Beam Length", // Optional 
 		graph_title: "Shear Force Diagram",
 		color: "red",
@@ -46,11 +45,6 @@ module.exports = async function (input_json) {
 			'Peak_Moment': {
 				'label': 'Peak Moment',
 				'value': peak_moment,
-				'units': 'kNm'
-			},
-			'Peak_Shear': {
-				'label': 'Peak Shear',
-				'value': peak_shear,
 				'units': 'kNm'
 			}
 		}
