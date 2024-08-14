5542a3182e7bdda89583c6ea4135491978c12d70
GeorgeChard <george.chard@skyciv.com>
2024-08-14 15:07:05 +1000

    Config and UI complete

--- calculate.js
+++ calculate.js
@@ -1,12 +1,12 @@
 module.exports = async function (input_json) {
 
-	let {first_input} = input_json; //Retrieve variables from config.json input
+	let {height, width, flange_width, web_width, vertical_axis_name, horizontal_axis_name} = input_json; //Retrieve variables from config.json input
 
 	//Calculations
-	let input_squared = first_input**2
+
 
 	//Reporting
-	ReportHelpers.print(`Reference`,`[math]Input^2 = ${input_squared} mm^2[math]`, ``)
+	// ReportHelpers.print(`Reference`,`[math]Input^2 = ${input_squared} mm^2[math]`, ``)
 
 	var output = {
 		'report': REPORT,
@@ -14,17 +14,17 @@ module.exports = async function (input_json) {
 			'results_heading': {
 				'label': 'Results',
 				'units': 'heading',
-			},
-			'output_name_1': {
-				'label': 'Input',
-				'value': first_input,
-				'units': 'mm'
-			},
-			'output_name_2': {
-				'label': 'Input<sup>2</sup>',
-				'value': input_squared,
-				'units': 'mm2'
-			},
+			}
+			// 'output_name_1': {
+			// 	'label': 'Input',
+			// 	'value': first_input,
+			// 	'units': 'mm'
+			// },
+			// 'output_name_2': {
+			// 	'label': 'Input<sup>2</sup>',
+			// 	'value': input_squared,
+			// 	'units': 'mm2'
+			// },
 		}
 	};
 
--- ui.js
+++ ui.js
@@ -1,4 +1,3 @@
-var creator = SVGCreator; //Create SVGCreator object
 var input = SKYCIV_DESIGN.designConfig.getInput(); //Retrieve input from HTML form
 
 var units = { //Default unit system
@@ -13,16 +12,124 @@ if (SKYCIV_DESIGN.units.getCurrentUnitSystem() == 'imperial') { //Convert units 
     units.M = 'kip/ft';
 }
 
-// Define graphic window size
-var total_width = jQuery('#ui-div').width(); //Set SVG total width (Find pixel size of graphic window from HTML)
-var total_height = 0.6 * total_width; //Set SVG total height
-var font_size = Math.round(0.06 * total_height); //Set default text size (scaled with window height)
+// --- Input Tab SVG Diagram ---
+function SectionDrawerExample(){
+    var creator = SVGCreator; //Create SVGCreator object
+    var total_width = jQuery('#input-div').width(); //Set SVG total width (Find pixel size of graphic window from HTML)
+    var total_height = 0.6 * total_width; //Set SVG total height
 
-// Initialise creator
-creator.initialize({ total_height: total_height, total_width: total_width, center: true });
+    // Initialise creator
+    creator.initialize({ total_height: total_height, total_width: total_width, center: true });
 
-// Output Diagram
-creator.endSVG();
-var html = creator.getSVGHtml();
-var ui_div = document.getElementById('ui-div');
-ui_div.innerHTML = html;
+    var axis = false; //If false, don't draw axis
+
+    if(input.show_axis){
+        Object.entries(input.axis_table).forEach(([key, value]) => { //Assign axis input table values to axis object 
+            axis = {
+                vertical_label: "Y",
+                horizontal_label: "Z",
+                vertical_color: value.vertical_axis_colour,
+                horizontal_color: value.horizontal_axis_colour,
+                vertical_axis_label_color: value.vertical_axis_colour,
+                horizontal_axis_label_color: value.horizontal_axis_colour,
+                axis_size: value.axis_size
+            }
+        })
+    }
+
+    var section_drawer = SectionDrawer();
+
+    svg_html = section_drawer.drawIBeam(
+        'black', //Section colour
+        input.width, //Section width
+        input.height, //Section height
+        input.flange_thickness, //Section flange thickness
+        input.web_thickness, //Section web thickness
+        300, //Graphic height (pixels)
+        200, //Graphic width (pixels)
+        `${units.L}`, //Unit system
+        axis, //Axis object
+        false //Section rotation
+    );
+
+    // Output Diagram
+    creator.endSVG();
+    var html = creator.getSVGHtml();
+    var ui_div = document.getElementById('input-div');
+    ui_div.innerHTML = html;
+}
+
+
+// --- Centre Tab ChartJS Graph ---
+function ChartJSExample(){
+
+    let labels_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] //Labels shown on X axis
+    let data_array = [] //Y axis plot points
+
+    for(i = 0; i < labels_array.length; i++){ //Calculate Y axis values
+        if(input.display_series == "exp"){
+            data_array.push(Math.exp(labels_array[i]))
+        }else{
+            data_array.push(Math.log(labels_array[i]))
+        }
+
+    }
+
+    var data = { //Chart graph object. Container for datasets
+        labels: labels_array,
+        datasets: [
+            {
+                label: `${input.data_label}`,
+                data:  data_array,
+                borderColor: 'rgb(54, 162, 235)',
+                backgroundColor: 'rgb(54, 162, 235)'
+            }
+        ]
+    };
+
+    //ChartJS Options
+    var chartConfiguration = {
+        type: 'line', //Graph type
+        data: data, //Chart graph object
+        options: {
+          responsive: true,
+          scales: { //Axes labels
+            x: {
+              display: true,
+              title: {
+                display: true,
+                text: `${input.x_label}`
+              }
+            },
+            y: {
+              display: true,
+              title: {
+                display: true,
+                text: `${input.y_label}`
+              }
+            }
+          },
+          plugins: {
+            legend: { //Legend options
+              position: 'bottom',
+            },
+            title: { //Graph title options (disabled in this case)
+              display: false,
+              text: 'Graph Title'
+            }
+          }
+        }
+    }
+
+    var canvas = "<canvas id ='canvas-id'></canvas>" //Create empty HTML canvas element
+
+    jQuery('#centre-div').empty(); //Empty graphics div
+    jQuery('#centre-div').append(canvas); //Create canvas element within graphics div
+    var canvas_element = document.getElementById('canvas-id').getContext('2d'); 
+    new Chart(canvas_element, chartConfiguration); //Add chart to Canvas
+
+}
+
+SectionDrawerExample();
+ChartJSExample();
+
